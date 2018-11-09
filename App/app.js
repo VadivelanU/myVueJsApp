36576f17a37fe61ed5be727a94cb1d68464e4bef
ODYSOLVD\Vadivelan.Udayakumar <vadivelan_udayakumar@odysseussolutions.com>
2018-11-09 20:33:29 +0530

    tutorial basics implementation

--- App/app.js
+++ App/app.js
@@ -1,6 +1,30 @@
 new Vue({
     el: "#myVueJsApp",
     data: {
-        helloWorldText:"Helloworld"
+        dateTimeInfo:"PreviousValue",
+        helloWorldText: "Helloworld",
+        imageUrl: "logo.png",
+        pInnerHtml: '<img src="logo.png" width="50" height="50" />',
+        pInnerHtmlNotWorking: '<img :src="imageUrl" width="50" height="50" />',
+    },
+    methods: {
+        greetUser: function () {
+            var hourPart = new Date().getHours();
+            if (hourPart < 11) {
+                return "Good Morning."
+            }
+            else if (hourPart < 16) {
+                return "Good Afternoon."
+            } else {
+                return "Good Evening."
+            }
+        },
+        getDateTimeInformation: function () {
+            var date = new Date();
+            dateTimeInfo = "Current Time : " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + " TimeZone-Offset: " + date.getTimezoneOffset() / 60;
+            console.log(dateTimeInfo);
+            //setTimeout(getDateTimeInformation(), 500);
+            return true;
+        }
     }
 });
